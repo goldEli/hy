7cf4a49e9ac30455686747427d8e48fedc3ae663
miaoyu <miaoyu@sefonsoft.com>
2021-03-02 13:05:17 +0800

    feat: remove and update post

--- server-nest/src/posts/posts.controller.ts
+++ server-nest/src/posts/posts.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 
 class CreatePostDto {
@@ -8,6 +16,12 @@ class CreatePostDto {
   content: string;
 }
 
+class UpdatePostDto {
+  @ApiProperty({ title: '帖子标题' })
+  title: string;
+  @ApiProperty({ title: '帖子内容' })
+  content: string;
+}
 @Controller('posts')
 @ApiTags('帖子')
 export class PostsController {
@@ -28,10 +42,33 @@ export class PostsController {
   }
 
   @Get(':id')
-  detail() {
+  @ApiOperation({ summary: '博客详情' })
+  detail(@Param('id') id: string) {
     return {
       status: 200,
-      content: 'asdfasfas',
+      data: {
+        id,
+        content: 123,
+      },
+    };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除博客' })
+  remove(@Param('id') id: string) {
+    return {
+      status: 200,
+      message: '删除成功',
+      data: id,
+    };
+  }
+  @Put(':id')
+  @ApiOperation({ summary: '修改博客' })
+  update(@Param('id') id: string, @Body() body: UpdatePostDto) {
+    return {
+      status: 200,
+      message: '修改成功',
+      data: body,
     };
   }
 }
